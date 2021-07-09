b1a983a15a6098e58cf42bd3d1ba9a550223de0c
OIer0528 <814603030@qq.com>
2021-07-09 14:45:18 +0800

    Add WordSegmentationBox component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,11 +13,20 @@
           show-word-limit
         >
         </el-input>
-        <el-button type="primary" plain>显示分词结果</el-button>
+
+        <el-button
+          type="primary"
+          plain
+          :disabled="wordSegmentationStatus == 'loading'"
+          @click="getWordSegmentationResult"
+        >
+          显示分词结果
+        </el-button>
         <el-button type="primary" plain>显示词性标注结果</el-button>
-        <div>
-          <p>{{ textAfterConversion }}</p>
-        </div>
+        <word-segmentation-box
+          :status="wordSegmentationStatus"
+          :results="wordSegmentationResults"
+        />
       </el-main>
       <el-footer><Footer /></el-footer>
     </el-container>
@@ -26,22 +35,33 @@
 
 <script>
 import Footer from "./components/Footer.vue";
-import { ref, computed } from "vue";
+import WordSegmentationBox from "./components/WordSegmentationBox.vue";
+import { ref } from "vue";
 
 export default {
   name: "App",
   components: {
     Footer,
+    WordSegmentationBox,
   },
   setup() {
     const textBeforeConversion = ref("");
-    const textAfterConversion = computed(() => {
-      return textBeforeConversion.value.split("");
-    });
+    const wordSegmentationStatus = ref("empty");
+    const wordSegmentationResults = ref([]);
+
+    const getWordSegmentationResult = () => {
+      wordSegmentationStatus.value = "loading";
+      setTimeout(() => {
+        wordSegmentationStatus.value = "ready";
+        wordSegmentationResults.value = textBeforeConversion.value.split("");
+      }, 1000);
+    };
 
     return {
       textBeforeConversion,
-      textAfterConversion,
+      wordSegmentationStatus,
+      wordSegmentationResults,
+      getWordSegmentationResult,
     };
   },
 };
@@ -64,6 +84,14 @@ body {
 #app .el-header {
   background: rgb(83, 168, 255);
   color: #ffffff;
+}
+
+#app .el-main .el-textarea {
+  margin-bottom: 1rem;
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  font-size: 1.2rem;
+  word-break: break-all;
 }
 
 #app .el-footer {
---
+++ frontend/src/components/WordSegmentationBox.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="word-segmentation-box">
+    <div v-if="status == 'empty'">
+      <!-- empty -->
+    </div>
+    <div v-else-if="status == 'loading'">
+      <p><i class="el-icon-loading"></i>正在获取分词结果...</p>
+    </div>
+    <div v-else>
+      <div class="desc">分词结果</div>
+      <div class="results-box">
+        <template v-for="word in results" :key="word">
+          <span class="word">{{ word }}</span>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WordSegmentationBox",
+  props: ["status", "results"],
+};
+</script>
+
+<style scoped>
+.word-segmentation-box {
+  margin-top: 1rem;
+}
+
+.results-box {
+  display: flex;
+  flex-wrap: wrap;
+  min-height: 5rem;
+  border: solid rgb(233, 233, 233) 1px;
+  border-radius: calc(1rem / 4);
+}
+
+.desc {
+  /* color: white; */
+  /* background-color: rgb(83, 168, 255); */
+  margin-bottom: 0.5rem;
+  border-radius: 0.5rem;
+  width: 70%;
+  margin-left: 15%;
+  margin-right: 15%;
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+
+.word {
+  border: solid rgb(179, 216, 255) 1px;
+  background-color: rgb(237, 246, 255);
+  padding: 0.1rem;
+  margin: 0.2rem 0.5rem;
+  height: 1rem;
+}
+
+.word:hover {
+  background-color: rgb(179, 216, 255);
+}
+</style>
